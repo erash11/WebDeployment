87975af7b76e51b843663e919477d206776ef98f
erash11 <68073911+erash11@users.noreply.github.com>
2024-06-12 16:46:54 -0500

    Update FB_BW_Dash.py

--- FB_BW_Dash.py
+++ FB_BW_Dash.py
@@ -99,4 +99,4 @@ def update_graph(selected_position):
     return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8050))
+    app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
